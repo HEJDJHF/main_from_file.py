5b4614a26330e75ef5779cc22b06fec658eb36ae
HEJDJHF <118367652+HEJDJHF@users.noreply.github.com>
2022-11-21 19:38:34 +0000

    Update main_from_user.py

--- main_from_user.py
+++ main_from_user.py
@@ -1,40 +1,47 @@
-import sys
-sys.path.append("./")
 from plotter import Plotter
-
 xmin = 1000000000
 ymin = 1000000000
 xmax = 1000000000
 ymax = 1000000000
 def isRayIntersectsSegment(poi,s_poi,e_poi):
-    if s_poi[1]==e_poi[1]:  #排除与射线平行、重合，线段首尾端点重合的情况
+    if s_poi[1]==e_poi[1]:  #Excluding parallel and coincident with the ray, the first and last endpoints of the line segment coincide
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
         return False
-    if s_poi[1]>poi[1] and e_poi[1]>poi[1]:  #线段在射线上方
+    if s_poi[1]>poi[1] and e_poi[1]>poi[1]:  #The line segment is above the ray
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
         return False
-    if s_poi[1]<poi[1] and e_poi[1]<poi[1]:  #线段在射线下方
+    if s_poi[1]<poi[1] and e_poi[1]<poi[1]:  #The line segment is below the ray
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
         return False
-    if s_poi[1]==poi[1] and e_poi[1]>poi[1]:  #交点为上端点，对应spoint
+    if s_poi[1]==poi[1] and e_poi[1]>poi[1]:  #The intersection point is the upper endpoint, corresponding to the spoint
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
         return False
-    if e_poi[1]==poi[1] and s_poi[1]>poi[1]:  #交点为下端点，对应spoint
+    if e_poi[1]==poi[1] and s_poi[1]>poi[1]:  #The intersection point is the lower endpoint, corresponding to the spoint
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
         return False
-    if s_poi[0]<poi[0] and e_poi[1]<poi[1]:  #线段在射线左边
+    if s_poi[0]<poi[0] and e_poi[1]<poi[1]:  #The line segment is to the left of the ray
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
         return False
 
-    xseg=e_poi[0]-(e_poi[0]-s_poi[0])*(e_poi[1]-poi[1])/(e_poi[1]-s_poi[1]) #求交
-    if xseg<poi[0]:  #交点在射线起点的左侧
+    xseg=e_poi[0]-(e_poi[0]-s_poi[0])*(e_poi[1]-poi[1])/(e_poi[1]-s_poi[1]) #Find the intersection point
+    # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
+    if xseg<poi[0]:  #The intersection point is to the left of the starting point of the ray
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
         return False
-    return True  #排除上述情况后
+    return True  #After ruling out the above
 def isPoiWithinPoly(poi,poly):
     global xmin
     global xmax
     global ymin
     global ymax
-    sinsc = 0  #交点数量
+    sinsc = 0  #Number of intersections
     x0 = poi[0]
     y0 = poi[1]
-    if x0<xmin and y0<ymin or x0>xmax and y0>ymax: return "outside"
+    if x0<xmin and y0<ymin or x0>xmax and y0>ymax: # Loop to judge whether the point is outside the smallest rectangle of the polygon create.
+        return "outside"
     for epoly in poly:
         for i in range(len(epoly)-1):  #[0,len-1]
+            # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
             s_poi = epoly[i]
             e_poi = epoly[i+1]
             x1 = s_poi[0]
@@ -42,16 +49,20 @@ def isPoiWithinPoly(poi,poly):
             x2 = e_poi[0]
             y2 = e_poi[1]
             if x0>=min(x1,x2) and x0<=max(x1,x2) and y0>=min(y1,y2) and y0<=max(y1,y2) and (y2-y0)*(x1-x0)==(y1-y0)*(x2-x0):
-                # 循环判断点是否在多边形上
+                # Loop to judge if a point is on a polygon.
                 return "boundary"
-        for epoly in poly:  #循环每条边点曲线->each polygon 是二维数组[[x1,y1],...[xn,yn]]
+        for epoly in poly:  #Loop each edge-point curve->each polygon is a two-dimensional array of[[x1,y1],...[xn,yn]]
+            # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
             for i in range(len(epoly)-1):  #[0,len-1]
                 s_poi = epoly[i]
                 e_poi = epoly[i+1]
-                if isRayIntersectsSegment(poi, s_poi, e_poi):
-                    sinsc+=1  #如果存在交点，则加1
+                if isRayIntersectsSegment(poi, s_poi, e_poi): # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
+                    sinsc+=1  # If an intersection exists, add 1
+                    # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
 
-        return "inside" if sinsc%2==1 else "outside"  #交点为基数，则在内部。交点为偶数，则在外部。
+        return "inside" if sinsc%2==1 else "outside"
+        # If the intersection is cardinal, it is on the inside. If the intersection point is even, it is outside.
+        # https://cloud.tencent.com/developer/article/1515808，29/09/2019，蛰虫始航
 def main():
     global xmin
     global xmax
@@ -63,7 +74,7 @@ def main():
     c = []
     plotter = Plotter()
     print('read polygon.csv')
-    with open("polygon.csv","r",encoding='utf-8') as f:  #读取polygon.csv
+    with open("polygon.csv","r",encoding='utf-8') as f:  #Import polygon.csv
         for i in f.readlines():
             i=i.strip().split(",")
             if i[0]=="id":
@@ -72,18 +83,21 @@ def main():
             xmax = max(xmax, float(i[1]))
             ymin = min(xmin, float(i[2]))
             ymax = max(ymax, float(i[2]))
-            poly_list.append([float(i[1]), float(i[2])])  #将坐标添加到列表里
+            poly_list.append([float(i[1]), float(i[2])])  #Add the coordinates to the list
             p_list[0].append([float(i[1]), float(i[2])])
-    n = int(input("请输入点的数量："))
-    for i in range(n):
-        x = float(input("x:"))
-        y = float(input("y:"))
-        input_list.append([x,y])  #将坐标添加到列表当中
+    try:
+        n = int(input("请输入点的数量："))
+        for i in range(n):
+            x = float(input("x:"))
+            y = float(input("y:"))
+            input_list.append([x,y])  #Add the coordinates to the list
+    except NameError:
+        print("请输入点的数量：")
     print('categorize points')
     for i in input_list:
-        c.append(isPoiWithinPoly(i,p_list))  #分类
+        c.append(isPoiWithinPoly(i,p_list))  #Classify
         print('write output.csv')
-    with open("output.csv","w",encoding='utf-8') as f:  #输出到csv
+    with open("output.csv","w",encoding='utf-8') as f:  #Output to csv file
             f.write("id"+","+"category"+"\n")
             for i in range(len(input_list)):
                 f.write(str(i+1)+"," +c[i]+ "\n")
@@ -93,14 +107,17 @@ def main():
     for point in poly_list:
         x.append(point[0])
         y.append(point[1])
-    plotter.add_polygon(x,y)  #画出多边形
+    plotter.add_polygon(x,y)  #Draw polygon
     a = []
     b = []
     for point in input_list:
         a.append(point[0])
         b.append(point[1])
     for i in range(len(input_list)):
-        plotter.add_point(a[i],b[i],c[i])  #画出检测点
+        plotter.add_point(a[i],b[i],c[i])  #Draw text points
+        import matplotlib.pyplot as plt  #https://matplotlib.org, John Hunter
+        plt.hlines(b[i], a[i], 8, 'orange', '-', label='ray')  #Draw ray
+        plt.legend(loc='lower right')
     plotter.show()
 
 if __name__== '__main__':
